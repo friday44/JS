c5b805b1988ffd826c3eae657762c254153b7b69
friday <friday0701@mail.ru>
2023-08-04 12:19:32 +0400

    js base

---
+++ less1/script.js
@@ -0,0 +1,48 @@
+// console.log(`Текст из script.js`);
+
+// let num = 123;
+// console.log(num);
+
+// let a = 5;
+// a = 7;
+// console.log(a);
+
+// let b = 1 + 2 + 3 + 4;
+// console.log(b);
+
+
+
+// let c = 10;
+// let d = 2;
+// let sum = c + d;
+// let deferens = c - d;
+// let product = c * d;
+// let quotient = c / d;
+
+// console.log(sum);
+// console.log(deferens);
+// console.log(product);
+// console.log(quotient);
+
+// console.log(sum + deferens + product + quotient);
+
+
+
+// let e = 1.5;
+// let f = 0.75;
+// console.log(e + f);
+
+// let j = 4;
+// console.log(-j);
+
+
+let name = 'TP';
+console.log(name);
+
+let java = 'java';
+let script = 'script';
+console.log(java + script);
+
+let hello = 'hello';
+let world = 'world';
+console.log(hello + ' ' + world);
